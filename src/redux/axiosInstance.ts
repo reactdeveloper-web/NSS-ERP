3c8938ade6109d7cf4cfb0aa567d366f7d23aca2
Nitesh <react.developer@narayanseva.org>
2026-04-13 16:50:35 +0530

    Update axiosInstance.ts

--- src/redux/axiosInstance.ts
+++ src/redux/axiosInstance.ts
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: '/erp/', // /erp/ // use proxy
-  baseURL: 'https://d23605ra7slvxy.cloudfront.net/erp/',
+  baseURL: '/erp/', // /erp/ // use proxy
+  // baseURL: 'https://deverp.narayanseva.org/erp/',
 });
 
 export default axiosInstance;
