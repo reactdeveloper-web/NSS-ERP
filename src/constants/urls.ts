b76549b2c84a786b85626c8378f6f4b73382a8ec
Nitesh <react.developer@narayanseva.org>
2026-04-13 17:18:18 +0530

    Update urls.ts

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,6 @@
 export const URL = {
   // baseAPIUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001',
-  baseAPIUrl: 'https://deverp.narayanseva.org/erp/',
+  baseAPIUrl: 'https://deverp.narayanseva.org',
   // baseAPIUrl: 'https://d23605ra7slvxy.cloudfront.net/erp/',
 };
 //https://deverp.narayanseva.org/erpapi
