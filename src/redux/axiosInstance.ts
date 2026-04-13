bf805a633dc06e9ad2fb0685ceb361704884ebd5
Nitesh <react.developer@narayanseva.org>
2026-04-13 16:36:42 +0530

    Update axiosInstance.ts

--- src/redux/axiosInstance.ts
+++ src/redux/axiosInstance.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/erp/', // /erp/ // use proxy
+  // baseURL: '/erp/', // /erp/ // use proxy
+  baseURL: 'https://d23605ra7slvxy.cloudfront.net/erp',
 });
 
 export default axiosInstance;
