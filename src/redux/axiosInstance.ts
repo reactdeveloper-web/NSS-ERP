81414347d0419f696d03817967e796926e05561c
Nitesh <react.developer@narayanseva.org>
2026-04-13 16:39:05 +0530

    Update axiosInstance.ts

--- src/redux/axiosInstance.ts
+++ src/redux/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: '/erp/', // /erp/ // use proxy
-  baseURL: 'https://d23605ra7slvxy.cloudfront.net/erp',
+  baseURL: 'https://d23605ra7slvxy.cloudfront.net/erp/',
 });
 
 export default axiosInstance;
